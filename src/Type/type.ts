a16e462d3b3a7ffd89aa5044292b754d24a23fd6
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-05-06 15:07:59 +0530

    union and any done

--- src/Type/type.ts
+++ src/Type/type.ts
@@ -22,7 +22,7 @@ let name = "Alice"; // here we are not explicitly specifying the type of name, b
 // ------------------------------------------------------
 
 let age: number = 25; // type anotation
-let name:string = "Alice"; // type inference
+let name: string = 'Alice'; // type inference
 
 // type anotation for function parameters and return type
 function multiply(a: number, b: number): number {
@@ -30,17 +30,38 @@ function multiply(a: number, b: number): number {
 }
 
 // type inference for function parameters and return type
-function greet(name: string) { // yaha humne name parameter ke type ko explicitly specify kiya h, lekin return type ko specify nahi kiya h, to typescript khud se infer kar lega ki return type string h
+function greet(name: string) {
+  // yaha humne name parameter ke type ko explicitly specify kiya h, lekin return type ko specify nahi kiya h, to typescript khud se infer kar lega ki return type string h
   return `Hello, ${name}!`;
 }
 
+// -----------------------------------------
 
+//! Union and any types
 
+//? Union type means when a variable can hold more than one type of value, we can use union types to specify that. We can use the pipe (|) symbol to separate the different types that a variable can hold. For example -
+let id: number | string; // here we are specifying that the variable id can hold either a number or a string
+id = 123; // valid
+id = 'abc'; // valid
+// id = true; // invalid - because we have not specified that id can hold a boolean value
 
+let apiResponseStatus: 'success' | 'pending' | 'error';
+apiResponseStatus = 'success'; // valid
+// apiResponseStatus = 'bhanu'; // invalid - because we have not specified that apiResponseStatus can hold a value other than 'success', 'pending', or 'error'
 
+// * ------------------------------------------------------------------
 
+//? Any type means when we want to allow a variable to hold any type of value,
+// this is usefull when we are not sure about the type of value that a variable will hold, or when we want to allow a variable to hold multiple types of values without having to specify them explicitly.
 
+// however, using the any type can be dangerous because it can lead to runtime errors if we try to access properties or call methods on a variable that holds a value of an unexpected type. Therefore, it is generally recommended to avoid using the any type and to use union types or other more specific types instead.
 
+let data: any; // here we are specifying that the variable data can hold any type of value
+data = 123;
+data = 'abc';
+data = true; // valid - because we have specified that data can hold any type of value
+// however, we should be careful when using the any type, because it can lead to runtime errors if we try to access properties or call methods on a variable that holds a value of an unexpected type. For example -
+data.toUpperCase(); // this will throw a runtime error if data holds a boolean value, because boolean values do not have the toUpperCase method
 
 // ------------------------------------------------------
 // Type Aliases
